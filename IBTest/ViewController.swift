58ee16b1ec2b0387e2b979a52106c2409ed80e6b
윤성호 <youn2029@naver.com>
2019-01-07 16:04:33 +0900

    Initial Commit

---
+++ IBTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBTest
+//
+//  Created by 윤성호 on 2019. 1. 7..
+//  Copyright © 2019년 윤성호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
